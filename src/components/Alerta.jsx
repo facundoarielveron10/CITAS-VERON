e3384f0716b9db70e958a6c3b7543de5ef94574c
Facundo Veron <facu_ve_2016@hotmail.com>
2022-10-04 00:03:17 -0300

    Agregado de tipo (error, exito) a alerta

--- src/components/Alerta.jsx
+++ src/components/Alerta.jsx
@@ -1,6 +1,16 @@
 function Alerta({ tipo, mensaje }) {
+  // Revisamos el tipo de alerta
+  if(tipo === 'error') {
+    tipo = 'bg-red-700 text-white text-center p-3 uppercase font-bold mb-3 rounded-md';
+  } else if(tipo === 'exito') {
+    tipo = 'bg-green-600 text-white text-center p-3 uppercase font-bold mb-3 rounded-md';
+  } else {
+    tipo = 'bg-orange-500 text-white text-center p-3 uppercase font-bold mb-3 rounded-md';
+  } 
+
   return (
-    <div className='bg-red-700 text-white text-center p-3 uppercase font-bold mb-3 rounded-md'>
+    <div 
+      className={tipo}>
         <p>{mensaje}</p>
     </div>
   )
